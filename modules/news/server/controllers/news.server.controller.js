21cc3c46d293b2e42a4b385227faf51f6588b3dc
Sana Komal <sanakomal93@gmail.com>
2016-09-25 08:29:40 +0500

    Setup end point for fetching all news

--- modules/news/server/controllers/news.server.controller.js
+++ modules/news/server/controllers/news.server.controller.js
@@ -3,11 +3,29 @@
 var path = require('path'),
     request = require('request'),
     mongoose = require('mongoose'),
+    config = require(path.resolve('./modules/global/apiConfig')),
     News = mongoose.model('News'),
     errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
 
 exports.fetch = function(req, res) {
-res.jsonp({
-    id: 1
+
+// End points. Move these to separate files. 
+// TODO: Possibly devise a way to shorten this and refactor this.
+var query = '?q=select%20*%20from%20cricket.news%20%20where%20region%3D%22in%22';
+var opts = '&diagnostics=true&format=json&env=store%3A%2F%2F0TxIGQMQbObzvU4Apia0V0';
+var url = config.api + query + opts;
+
+request.get({
+    url: url,
+    json: req.payload,
+    headers: {
+        Accept: 'application/json'
+    }
+}, (err, httpResponse, body) => {
+    if (err) {
+        res.jsonp(err);
+    } else {
+        res.jsonp(body);
+    }
 });
 };
