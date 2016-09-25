8f81501bc78ffd5ffbbe740d68931c1bc2c4e6f7
Sana Komal <sanakomal93@gmail.com>
2016-09-25 08:07:45 +0500

    Add request to package.json

--- modules/news/server/controllers/news.server.controller.js
+++ modules/news/server/controllers/news.server.controller.js
@@ -1,6 +1,7 @@
 // Setup controller here.
 
 var path = require('path'),
+    request = require('request'),
     mongoose = require('mongoose'),
     News = mongoose.model('News'),
     errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
